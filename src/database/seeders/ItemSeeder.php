bf9a71b0bf0581b35b0487353730824da24a5023
sugi <tales10-orix51-saiko@outlook.jp>
2026-05-05 08:04:04 +0900

    詳細

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -19,7 +19,7 @@ class ItemSeeder extends Seeder
          'price' => 15000,
          'brand' => 'Rolax',
          'description' => 'スタイリッシュな腕時計',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
          'condition' => '良好'
     ]);
 
@@ -28,7 +28,7 @@ class ItemSeeder extends Seeder
          'price' => 5000,
          'brand' => '西芝',
          'description' => '高速で信頼性の高いハードディスク',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
          'condition' => '目立った傷や汚れなし'
     ]);
         Item::create([
@@ -36,7 +36,7 @@ class ItemSeeder extends Seeder
          'price' => 300,
          'brand' => 'なし',
          'description' => '新鮮な玉ねぎ３束のセット',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
          'condition' => 'やや傷や汚れあり'
     ]);
         Item::create([
@@ -44,7 +44,7 @@ class ItemSeeder extends Seeder
          'price' => 4000,
          'brand' => '',
          'description' => 'クラシックなデザインの革靴',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
          'condition' => '状態が悪い'
     ]);  
         Item::create([
@@ -52,7 +52,7 @@ class ItemSeeder extends Seeder
          'price' => 45000,
          'brand' => '',
          'description' => '高性能なノートパソコン',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
          'condition' => '良好'
     ]);
         Item::create([
@@ -60,7 +60,7 @@ class ItemSeeder extends Seeder
          'price' => 8000,
          'brand' => 'なし',
          'description' => '高音質のレコーディング用マイク',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
          'condition' => '目立った傷や汚れなし'
     ]);  
         Item::create([
@@ -68,7 +68,7 @@ class ItemSeeder extends Seeder
          'price' => 3500,
          'brand' => '',
          'description' => 'おしゃれなショルダーバッグ',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
          'condition' => 'やや傷や汚れあり'
     ]);
         Item::create([
@@ -76,7 +76,7 @@ class ItemSeeder extends Seeder
          'price' => 500,
          'brand' => 'なし',
          'description' => '使いやすいタンブラー',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
          'condition' => '状態が悪い'
     ]); 
         Item::create([
@@ -84,7 +84,7 @@ class ItemSeeder extends Seeder
          'price' => 4000,
          'brand' => 'Starbacks',
          'description' => '手動のコーヒーミル',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
          'condition' => '良好'
     ]);
         Item::create([
@@ -92,7 +92,7 @@ class ItemSeeder extends Seeder
          'price' => 2500,
          'brand' => '',
          'description' => '便利なメイクアップセット',
-         'img_url' => 'https://picsum.photos/200',
+         'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
          'condition' => '目立った傷や汚れなし'
     ]);
     }
--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -7,12 +7,17 @@
 
   <div class="item-info">
     <h2>{{ $item->name }}</h2>
-    <p class="price">¥{{ number_format($item->price) }}</p>
+    <p>¥{{ number_format($item->price) }}</p>
 
+    <button class="buy-btn">購入手続きへ</button>
+
+    <h3>商品説明</h3>
     <p>{{ $item->description }}</p>
+
     <p>ブランド：{{ $item->brand }}</p>
     <p>状態：{{ $item->condition }}</p>
   </div>
+
 </div>
 
 @endsection
