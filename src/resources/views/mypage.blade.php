40f248045399d698212195de2238b5e2a32925bd
sugi <tales10-orix51-saiko@outlook.jp>
2026-05-24 15:07:28 +0900

    ユーザー情報

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -8,16 +8,26 @@
 
       <h2>{{ $user->name }}</h2>
 
+      <a href="/mypage/profile">
+        プロフィールを編集
+      </a>
+
       <h3>出品した商品</h3>
 
   @foreach($sellItems as $item)
-     <p>{{ $item->name }}</p>
+     <div class="item-card">
+       <img src="{{ $item->img_url }}">
+       <p>{{ $item->name }}</p>
+     </div>
   @endforeach
 
       <h3>購入した商品</h3>
 
   @foreach($purchasedItems as $item)
-    <p>{{ $item->name }}</p>
+    <div class="item-card">
+       <img src="{{ $item->img_url }}">
+       <p>{{ $item->name }}</p>
+    </div>
   @endforeach
       
   </div>
