cd18f347c7a5da0a420e04f14aebf236a0b11fb2
sugi <tales10-orix51-saiko@outlook.jp>
2026-06-08 09:35:32 +0900

    LogoutTest

---
+++ src/tests/Feature/LogoutTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class LogoutTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_user_can_logout()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->post('/logout');
+
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+}
