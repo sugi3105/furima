2dd6e23829c11d47b1a04947048ed19747ae3cfe
sugi <tales10-orix51-saiko@outlook.jp>
2026-06-25 14:18:47 +0900

    入力保持

--- src/resources/views/items/create.blade.php
+++ src/resources/views/items/create.blade.php
@@ -30,7 +30,8 @@
 
           <input type="checkbox"
              name="categories[]"
-             value="{{ $category->id }}">
+             value="{{ $category->id }}"
+             {{ in_array($category->id, old('categories', [])) ? 'checked' : '' }}>
 
           <span>{{ $category->name }}</span>
          </label>
@@ -45,18 +46,19 @@
 
         <option value="">選択してください</option>
 
-        <option value="良好">良好</option>
+        <option value="良好"
+            {{ old('condition') == '良好' ? 'selected' : '' }}>良好</option>
 
-        <option value="目立った傷や汚れなし">
-          目立った傷や汚れなし
+        <option value="目立った傷や汚れなし"
+            {{ old('condition') == '目立った傷や汚れなし' ? 'selected' : '' }}>目立った傷や汚れなし</option>
         </option>
 
-        <option value="やや傷や汚れあり">
-          やや傷や汚れあり
+        <option value="やや傷や汚れあり"
+             {{ old('condition') == 'やや傷や汚れあり' ? 'selected' : '' }}>やや傷や汚れあり</option>
         </option>
 
-        <option value="状態が悪い">
-          状態が悪い
+        <option value="状態が悪い"
+              {{ old('condition') == '状態が悪い' ? 'selected' : '' }}>状態が悪い</option>
         </option>
 
       </select>
